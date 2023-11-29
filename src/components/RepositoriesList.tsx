718a0202c63c4a85c89346f281c43e8bf840f248
Dave Dankel <ddankel@gmail.com>
2023-11-29 00:24:22 -0600

    Hook up typed selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import useActions from "../hooks/useActions";
+import useTypedSelector from "../hooks/useTypedSelector";
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,6 +18,16 @@ const RepositoriesList = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data && (
+        <>
+          <h3>Matching Packages:</h3>
+          {data.map((dataItem) => (
+            <div key={dataItem}>{dataItem}</div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
